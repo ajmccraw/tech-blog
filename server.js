2dc4a443fb6e6284a9a6aaee3388ccf3846b78b6
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-10-02 11:49:11 -0500

    adding files

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// grant access to handlebars helpers functions
+const helpers = require('./utils/helpers');
+
+// set Handlebars as this app's template engine with the following lines of code
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// create express-session and connect-session-sequelize
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+// The "sync" part means that this is Sequelize taking the models 
+// and connecting them to associated database tables
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
